0eded651b68c4cb863d5d9842780dfe089cf6252
Joao Teixeira <jaobacanao@gmail.com>
2023-06-29 23:24:44 -0300

    adding hover on dropdown

--- src/Components/Dropdown.tsx
+++ src/Components/Dropdown.tsx
@@ -4,36 +4,54 @@ import { useEffect, useState } from "react";
 
 const Container = styled.div`
 
-    display: inline-flex;
+    display: flex;
+    flex-direction: column;
     align-items: center;
     font-family: 'Poppins', sans-serif;
     cursor: pointer;
+
+    .dropdown-label{
+        display: inline-flex;
+    }
 
     .icon{
         transition: transform 0.2s;
     }
 
+    .dropdown-content{
+
+        position: absolute;
+        top: 100px;
+        transition: ease-in 0.5s;
+    }
+
     :hover{
         .icon{
-            
             transform: rotate(90deg);
+        }
+        .dropdown-content{
+            transform: ease-in 0.5s;
         }
     }
 `
 
 export function Dropdown() {
     const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-
-    const dropdown = document.querySelector(".dropdown");
-
     
-
-
     return (
-        <Container className="dropdown" >
-            Social Medias
-            <ChevronRightIcon className="icon" />
-            
+        <Container className="dropdown" 
+        onMouseEnter={() => setIsDropdownOpen(true)}
+        onMouseLeave={() => setIsDropdownOpen(false)}
+        >
+            <div className="dropdown-label">
+                Social Medias
+                <ChevronRightIcon className="icon" />
+            </div>
+            {isDropdownOpen == true && (
+                <div className="dropdown-content">
+                    <a href="#">Facebook</a>
+                </div>
+            )}
         </Container>
     );
 }
