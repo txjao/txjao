0b5f39185eb01bef2e05dcb0d787a2c4e52841d9
Joao Teixeira <jaobacanao@gmail.com>
2023-07-02 21:04:35 -0300

    implementing typed.js lib

--- src/Components/Lettering.tsx
+++ src/Components/Lettering.tsx
@@ -1,9 +1,8 @@
+import { useEffect, useState } from "react";
 import styled from "styled-components";
+import { Typed } from "typed.ts";
 
 const Container = styled.div`
-
-    max-width: 700px;
-
    h1, h2{
     background: -webkit-linear-gradient(180deg, rgba(255,240,0,1) 0%, rgba(0,181,255,1) 100%);
     -webkit-background-clip: text;
@@ -19,12 +18,48 @@ const Container = styled.div`
 
 
 export function Lettering() {
+    const [text, setText] = useState('');
+
+    const line1 = 'Hello, World!';
+    const line2 = 'slow';
+    const line3 = 'this is typed really fast, but errors are slow';
+    const line4 = 'this line is fast forwarded. No errors will be made';
+
+
+
+
+    useEffect(() => {
+
+        const typed = new Typed({
+            callback: text => {
+                setText(text)
+                console.log(text)
+            }
+        });
+
+        async () => {
+            typed
+                .type(line1)
+                .backspace(line1.length)
+                .type(line2, { perLetterDelay: { min: 200, max: 400 } })
+                .backspace(line2.length)
+                .type(line3, { eraseDelay: { min: 40, max: 80 }, perLetterDelay: { min: 200, max: 400 } })
+                .backspace(line3.length);
+
+            typed.fastForward();
+            await typed.run();
+            await typed.reset(true);
+            typed.type(line4);
+            await typed.run();
+        }
+    }, [])
+
     return (
         <Container>
-            <div style={{display: "inline-flex"}}>
-            <h1>Hi there!</h1><p>👋</p>
+            <div style={{ display: "inline-flex" }}>
+                <h1>Hi there!</h1><p>👋</p>
             </div>
-            <h2>My name is João, im 20 years old!</h2>
+            <h2>My name is João, im {text}</h2>
         </Container>
     );
 }
