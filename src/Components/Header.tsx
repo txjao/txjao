2c45a7c9c8e3114e6f02f94280fb8fc9ed360f46
Joao Teixeira <jaobacanao@gmail.com>
2023-09-22 16:07:09 -0300

    feat: adding breakpoint

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,8 +1,9 @@
 import styled from "styled-components";
 import { Toggle } from "./Toggle";
 import { Dropdown } from "./Dropdown";
+import { useEffect, useState } from "react";
 
-const Container = styled.header`
+const ContainerDesktop = styled.header`
     display: flex;
     justify-content: space-between;
     align-items: center;
@@ -14,6 +15,7 @@ const Container = styled.header`
         font-family: 'Poppins', sans-serif;
         text-decoration: none;
         color: #000;
+        transition: font-weight 0.2s ease;
     }
 
     img{
@@ -32,18 +34,82 @@ const Container = styled.header`
     a:hover::after {
         width: 100%;
     }
+
+    #language-button:hover{
+        font-weight: 500;
+    }
 `
+
+const ContainerMobile = styled.header`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: #FAFAFA;
+    height: 96px;
+    padding: 0 60px;
+
+    a{
+        font-family: 'Poppins', sans-serif;
+        text-decoration: none;
+        color: #000;
+        transition: font-weight 0.2s ease;
+    }
+
+    img{
+        max-width: 48px;
+    }
+
+    a::after {
+        content: '';
+        display: block;
+        width: 0;
+        height: 1px;
+        background: #000;
+        transition: width .2s ease-in;
+    }
+
+    a:hover::after {
+        width: 100%;
+    }
+
+    #language-button:hover{
+        font-weight: 500;
+    }
+`
+
 export function Header() {
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        if (window.innerWidth < 768) {
+            setIsMobile(true)
+        } else {
+            setIsMobile(false)
+        }
+    }, [])
 
     return (
-        <Container>
-            <Toggle/>
-            <a href="https://www.linkedin.com/in/jo%C3%A3o-victor-teixeira-4b1429195/">Linkedin</a>
-            <a href="https://github.com/txjao">Github</a>
-            <img src="/favicon.png" />
-            <Dropdown/>
-            <a href="/">Resume</a>
-            <button>PT</button>
-        </Container>
+        <>
+            { !isMobile && (
+                    <ContainerDesktop>
+                        <Toggle />
+                        <a href="https://www.linkedin.com/in/jo%C3%A3o-victor-teixeira-4b1429195/">Linkedin</a>
+                        <a href="https://github.com/txjao">Github</a>
+                        <img src="/favicon.png" />
+                        <Dropdown />
+                        <a href="/">Resume</a>
+                        <a href="/" id="language-button">PT</a>
+                    </ContainerDesktop >
+                )
+            }
+            { isMobile && (
+                    <ContainerMobile>
+                        <img src="/favicon.png" />
+
+
+                    </ContainerMobile >
+                )
+            }
+        </>
     );
 }
