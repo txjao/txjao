5e0473126020436c5559feb38fdf7fc37f05f504
João <jaobacanao@gmail.com>
2023-10-16 16:36:05 -0300

    fix: fixing deploy

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,9 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
 import App from "./App";
 
-const router = createBrowserRouter([
-  {
-    path: "/en",
-    element: <App/>,
-  },
-]);
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App/>
   </React.StrictMode>,
 )
