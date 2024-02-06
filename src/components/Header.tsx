8abb8e82b4cc1d3606910306c946517fbebb40c3
Joao Teixeira <jaobacanao@gmail.com>
2024-02-05 23:22:18 -0300

    feat: styled header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,18 @@
-import { Dropdown } from "./Dropdown";
+'use client'
 
+import { Dropdown } from "./Dropdown";
 
 export function Header() {
     return (
-        <header >
+        <header className="flex w-full justify-between px-8">
             {/* <Toggle /> */}
             <a href="https://www.linkedin.com/in/jo%C3%A3o-victor-teixeira-4b1429195/">Linkedin</a>
             <a href="https://github.com/txjao">Github</a>
             <img src="/favicon.png" />
-            <Dropdown options={
-                [
-                    "English",
-                    "Portuguese"
-                ]
-            }/>
+            <Dropdown title="Contact Me">
+                <a href="https://wa.me/31995985251" target="_blank" rel="noopener">Whatsapp</a>
+                <a href="mailto: contatojoaovteixeira@gmail.com">Email</a>
+            </Dropdown>
             <a id="language-button">PT</a>
         </header>
     );
