c4361cbed1c834055b8efbb3e9c48a56ba619432
Joao Teixeira <jaobacanao@gmail.com>
2024-03-07 10:33:49 -0300

    feat: add dropdown component

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,20 +1,30 @@
-
-type dropdown = {
-    title: string;
-    href: string;
-}
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import { useState } from "react";
 
 interface DropdownProps {
-    options: dropdown[];
+    children?: React.ReactNode;
+    title?: string;
 }
 
 export function Dropdown(props: DropdownProps) {
+    const [isOpen, setIsOpen] = useState(false);
 
     return (
-        <div>
-            {props.options.map((option, index) => {
-                return <a key={index} target="_blank" href="">{option}</a>
-            })}
+        <div className="flex flex-col"
+            onMouseEnter={() => setIsOpen(true)}
+            onMouseLeave={() => setIsOpen(false)}
+        >
+            {props.title &&
+                <div className="dropdown-label">
+                    <span>{props.title}</span>
+                    <ChevronRightIcon className={`${isOpen ? 'rotate-90 transition-all duration-200' : 'rotate-0 transition-all duration-200'}`}/>
+                </div>
+            }
+            {isOpen && (
+                <div className="flex flex-col object-cover *:flex *:flex-col *:w-min *:after:w-0 *:after:bg-black *:after:h-px *:after:hover:w-full *:after:hover:transition-all *:after:hover:duration-500">
+                    {props.children}
+                </div>
+            )}
         </div>
     )
 }
