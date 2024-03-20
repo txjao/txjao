979cff578f108387a64e6eefadf262855eb22d8b
Joao Teixeira <jaobacanao@gmail.com>
2024-03-20 13:14:04 -0300

    feat: add new interface and improves icon importing

--- src/Components/Info.tsx
+++ src/Components/Info.tsx
@@ -1,12 +1,7 @@
 import styled from "styled-components";
 import { Lettering } from "./Lettering";
 import { ReactSVG } from 'react-svg'
-import linkedin from '../assets/linkedin.svg'
-import github from '../assets/github.svg'
-import instagram from '../assets/instagram.svg'
-import spotify from '../assets/spotify.svg'
-import twitter from '../assets/twitter.svg'
-import discord from '../assets/discord.svg'
+import socialMediaIcons from '../utils/SocialMediaIcons';
 import { useState } from "react";
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
@@ -108,13 +103,13 @@ export function Info() {
                 </div>
                 <p>Im a software engineer learning full-stack development, currently working as a Front-end Developer.</p>
                 <div className="icons">
-                    <a target="_blank" href="https://www.linkedin.com/in/jo%C3%A3o-victor-teixeira-4b1429195/"> <ReactSVG src={linkedin} /></a>
-                    <a target="_blank" href="https://github.com/txjao"> <ReactSVG src={github} /></a>
-                    <a target="_blank" href="https://www.instagram.com/tx.jsx/"> <ReactSVG src={instagram} /></a>
-                    <a target="_blank" href="https://open.spotify.com/user/lzx7pb5mnpd5dd47m99carpk9?si=664bbc8ce7c74a69"> <ReactSVG src={spotify} /></a>
-                    {easterEgg > 10 && (<a target="_blank" href="https://twitter.com/tttexera"> <ReactSVG src={twitter} /></a>)}
+                    <a target="_blank" href="https://www.linkedin.com/in/jo%C3%A3o-victor-teixeira-4b1429195/"> <ReactSVG src={socialMediaIcons.linkedin} /></a>
+                    <a target="_blank" href="https://github.com/txjao"> <ReactSVG src={socialMediaIcons.github} /></a>
+                    <a target="_blank" href="https://www.instagram.com/tx.jsx/"> <ReactSVG src={socialMediaIcons.instagram} /></a>
+                    <a target="_blank" href="https://open.spotify.com/user/lzx7pb5mnpd5dd47m99carpk9?si=664bbc8ce7c74a69"> <ReactSVG src={socialMediaIcons.spotify} /></a>
+                    {easterEgg > 10 && (<a target="_blank" href="https://twitter.com/tttexera"> <ReactSVG src={socialMediaIcons.twitter} /></a>)}
                     <a onClick={() => handleOpen()}>
-                        <ReactSVG src={discord} />
+                        <ReactSVG src={socialMediaIcons.discord} />
                     </a>
                 </div>
                 <Dialog
---
+++ src/utils/SocialMediaIcons.ts
@@ -0,0 +1,17 @@
+import linkedin from '../assets/linkedin.svg';
+import github from '../assets/github.svg';
+import instagram from '../assets/instagram.svg';
+import spotify from '../assets/spotify.svg';
+import twitter from '../assets/twitter.svg';
+import discord from '../assets/discord.svg';
+
+const socialMediaIcons = {
+    linkedin,
+    github,
+    instagram,
+    spotify,
+    twitter,
+    discord
+};
+
+export default socialMediaIcons;
