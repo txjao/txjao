7a71c635fc8ac768cb759a44303875017e45b9c0
joaoteixeira <jaobacanao@gmail.com>
2026-01-31 18:11:57 -0300

    chore: setup the project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import { Header } from "@/components/Header";
 
 export default function Home() {
   return (
-    <main>
-      <Header />
-    </main>
+    <div className="">
+      <main className="">
+
+        sadljkhaskljdgkla
+      </main>
+    </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  content: [
+    "./app/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-jost)', ...fontFamily.sans], // substitui o sans padrão
+      },
+    },
+  },
+};
